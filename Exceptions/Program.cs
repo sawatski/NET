686423d954d2c9f0df732ef4ec0618041673e3b8
sawatski <lyoshayarosh@gmail.com>
2021-06-23 17:07:38 +0300

    Exceptions

---
+++ Exceptions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Labs
+{
+    class Program
+    {
+        static void ProcessString(string s)
+        {
+            if (s == null)
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        public static void Main()
+        {
+            try
+            {
+                string s = null;
+                ProcessString(s);
+                ProcessString("gkhbrsk");
+            }
+            // Специфика:
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("{0} Первое исключение.", e);
+            }
+            // Рандом:
+            catch (Exception e)
+            {
+                Console.WriteLine("{0} Второе исключение.", e);
+            }
+            finally
+            {
+                Console.WriteLine("Исключений не было");
+            }
+        }
+    }
+}
